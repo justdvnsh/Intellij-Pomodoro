42aa804a2ffea0eb9d202db0d9afb12b53a02801
GitHub Action <action@github.com>
2022-12-22 19:26:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/justdvnsh/intellijpomodoro/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/justdvnsh/intellijpomodoro/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.justdvnsh.intellijpomodoro.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.justdvnsh.intellijpomodoro.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/justdvnsh/intellijpomodoro/services/MyProjectService.kt
+++ src/main/kotlin/com/github/justdvnsh/intellijpomodoro/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.justdvnsh.intellijpomodoro.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.justdvnsh.intellijpomodoro.MyBundle
 
 class MyProjectService(project: Project) {
 
